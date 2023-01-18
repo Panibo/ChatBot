ffbb4e534590310231e715bd214af7879675a87f
Panibo <miro.mariapori@metropolia.fi>
2023-01-18 09:53:35 +0200

    FXML Setup

---
+++ src/layout/UIController.java
@@ -0,0 +1,12 @@
+package layout;
+import javafx.event.Event;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class UIController {
+    @FXML
+    public void buttonClicked(Event e){
+        System.out.println("Button clicked");
+    }
+
+}
